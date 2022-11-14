8f4fd266ed87c348f9ca7857a124cddaba1cf3f0
maxwellchaos <maxwellchaos@gmail.com>
2022-11-14 11:49:55 +0300

    добавлен тест полиморфизма

---
+++ FigureClassLibrary/FigureClassLibraryTest/IFigureTest.cs
@@ -0,0 +1,22 @@
+﻿using FigureClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigureClassLibraryTest
+{
+    public class IFigureTest
+    {
+        [Fact]
+        public void PolimorfizmTest()
+        {
+            IFigure circle = new Circle(1.1);
+            IFigure triangle = new Triangle(3, 4, 5);
+
+            Assert.Equal(1.1 * 1.1 * Math.PI, circle.Square());
+            Assert.Equal(3 * 4 / 2, triangle.Square());
+        }
+    }
+}
