64a50aa6a1352a47336259505d25553ac76e60d7
maxwellchaos <maxwellchaos@gmail.com>
2022-11-14 11:09:45 +0300

    добавлен тест класса многоугольника

---
+++ FigureClassLibrary/FigureClassLibraryTest/PoligonTest.cs
@@ -0,0 +1,24 @@
+﻿using AutoFixture.Xunit2;
+using FigureClassLibrary;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigureClassLibraryTest
+{
+    public class PoligonTest
+    {
+        [Theory,AutoData]
+        public void PerimetrTest(List<double> edges)
+        {
+            var mock = new Mock<Poligon>();
+            mock.Setup(polygon => polygon.Edges()).Returns(edges);
+            Poligon poligon = mock.Object;
+
+            Assert.Equal(edges.Sum(), poligon.Perimetr());
+        }
+    }
+}
