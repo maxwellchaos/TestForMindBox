3497c1f0a8855c76ec26341a0237bf7a2a04fd62
maxwellchaos <maxwellchaos@gmail.com>
2022-11-14 11:21:20 +0300

    добавлен тест круга

---
+++ FigureClassLibrary/FigureClassLibraryTest/CircleTest.cs
@@ -0,0 +1,37 @@
+﻿using AutoFixture.Xunit2;
+using FigureClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigureClassLibraryTest
+{
+    public class CircleTest
+    {
+        [Theory,AutoData]
+        public void NegativeRadiusTest(double radius)
+        {
+             radius = -Math.Abs(radius);
+
+             Assert.Throws<ArgumentException>(()=>new Circle(radius));
+        }
+
+        [Fact]
+        public void SetGetRadiusTest()
+        {
+            Circle circle = new Circle(1.1);
+
+            Assert.Equal(1.1, circle.Radius);
+        }
+
+        [Theory,AutoData]
+        public void SquareTest(double radius)
+        {
+            Circle circle = new Circle(Math.Abs(radius));
+
+            Assert.Equal(radius * radius * Math.PI, circle.Square());
+        }
+    }
+}
